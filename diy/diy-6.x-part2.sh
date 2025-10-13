25db5e6e149a3139a6f23d1e56d4f728eb5bca4e
Kwonelee <995546054@qq.com>
2025-10-13 11:53:44 +0800

    Update diy-6.x-part2.sh

--- diy/diy-6.x-part2.sh
+++ diy/diy-6.x-part2.sh
@@ -27,6 +27,8 @@ TARGET_DEVICES += tvi_tvi3315a" >> target/linux/rockchip/image/armv8.mk
 cp -f $GITHUB_WORKSPACE/configfiles/uboot-rockchip/Makefile package/boot/uboot-rockchip/Makefile
 
 # 复制U-Boot补丁到package/boot/uboot-rockchip/patches
+cp -f $GITHUB_WORKSPACE/configfiles/patch/001-disable-efi-loader.patch package/boot/uboot-rockchip/patches/
+cp -f $GITHUB_WORKSPACE/configfiles/patch/001-rockchip-saradc-supports-rk3568.patch package/boot/uboot-rockchip/patches/
 cp -f $GITHUB_WORKSPACE/configfiles/patch/101-rockchip-add-TVI-TVI3315A.patch package/boot/uboot-rockchip/patches/
 
 # 复制内核补丁到target/linux/rockchip/patches-6.6
