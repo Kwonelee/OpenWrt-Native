d7dd74f0df21314d1f029a9702c6a792705aea01
Kwonelee <995546054@qq.com>
2025-10-13 22:00:47 +0800

    Update 00-prepare_add_device.sh

--- diy/00-prepare_add_device.sh
+++ diy/00-prepare_add_device.sh
@@ -34,22 +34,27 @@ cp -f $GITHUB_WORKSPACE/configfiles/patch/128-arm64-dts-rockchip-rk3399-Add-TVI3
 # 移植RK3566示例，其他RK35xx可模仿
 # ================================================================
 # 增加station-m2设备
-#echo -e "\\ndefine Device/firefly_station-m2
-  #DEVICE_VENDOR := Firefly
-  #DEVICE_MODEL := Station M2 / RK3566 ROC PC
-  #SOC := rk3566
-  #DEVICE_DTS := rockchip/rk3566-roc-pc
-  #SUPPORTED_DEVICES += firefly,station-m2 firefly,rk3566-roc-pc
-  #UBOOT_DEVICE_NAME := station-m2-rk3566
-  #DEVICE_PACKAGES := kmod-nvme kmod-scsi-core
-#endef
-#TARGET_DEVICES += firefly_station-m2" >> target/linux/rockchip/image/armv8.mk
+echo -e "\\ndefine Device/firefly_station-m2
+  DEVICE_VENDOR := Firefly
+  DEVICE_MODEL := Station M2 / RK3566 ROC PC
+  SOC := rk3566
+  DEVICE_DTS := rockchip/rk3566-roc-pc
+  SUPPORTED_DEVICES += firefly,station-m2 firefly,rk3566-roc-pc
+  UBOOT_DEVICE_NAME := station-m2-rk3566
+  DEVICE_PACKAGES := kmod-nvme kmod-scsi-core
+endef
+TARGET_DEVICES += firefly_station-m2" >> target/linux/rockchip/image/armv8.mk
 
-# 复制U-Boot补丁到package/boot/uboot-rockchip/patches
-#
+# 复制uboot配置、dts到package/boot/uboot-rockchip
+mkdir -p package/boot/uboot-rockchip/src/arch/arm/dts
+mkdir -p package/boot/uboot-rockchip/src/configs
+cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3568/rk3566-roc-pc.dts package/boot/uboot-rockchip/src/arch/arm/dts/
+cp -f $GITHUB_WORKSPACE/configfiles/uboot-rockchip/rk3566-station-m2-u-boot.dtsi package/boot/uboot-rockchip/src/arch/arm/dts/
+cp -f $GITHUB_WORKSPACE/configfiles/uboot-rockchip/station-m2-rk3566_defconfig package/boot/uboot-rockchip/src/configs/
 
-# 复制内核补丁到target/linux/rockchip/patches-6.6
-#
+# 复制dts到files/arch/arm64/boot/dts/rockchip
+mkdir -p target/linux/rockchip/files/arch/arm64/boot/dts/rockchip
+cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3568/rk3566-roc-pc.dts target/linux/rockchip/files/arch/arm64/boot/dts/rockchip/
 # ================================================================
 # RK35xx示例结束
 # ================================================================
