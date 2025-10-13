25baf9516291a2f717571576228e159f7fc0aa36
Kwonelee <995546054@qq.com>
2025-10-13 21:29:39 +0800

    Update 02-rockchip_target_only.sh

--- diy/02-rockchip_target_only.sh
+++ diy/02-rockchip_target_only.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -e
 
 # 使用特定的优化
-#sed -i 's,-mcpu=generic,-march=armv8-a+crc+crypto,g' include/target.mk
-#sed -i 's,kmod-r8168,kmod-r8169,g' target/linux/rockchip/image/armv8.mk
+sed -i 's,-mcpu=generic,-march=armv8-a+crc+crypto,g' include/target.mk
+sed -i 's,kmod-r8168,kmod-r8169,g' target/linux/rockchip/image/armv8.mk
 
 # Vermagic
-#wget https://downloads.openwrt.org/releases/24.10.3/targets/rockchip/armv8/profiles.json
-#jq -r '.linux_kernel.vermagic' profiles.json >.vermagic
-#sed -i -e 's/^\(.\).*vermagic$/\1cp $(TOPDIR)\/.vermagic $(LINUX_DIR)\/.vermagic/' include/kernel-defaults.mk
+wget https://downloads.openwrt.org/releases/24.10.3/targets/rockchip/armv8/profiles.json
+jq -r '.linux_kernel.vermagic' profiles.json >.vermagic
+sed -i -e 's/^\(.\).*vermagic$/\1cp $(TOPDIR)\/.vermagic $(LINUX_DIR)\/.vermagic/' include/kernel-defaults.mk
 
 # Distfeeds.conf
 mkdir -p files/etc/opkg
@@ -35,4 +35,3 @@ EOF
 #wget -qO- $GEOIP_URL > files/etc/openclash/GeoIP.dat
 #wget -qO- $GEOSITE_URL > files/etc/openclash/GeoSite.dat
 #chmod +x files/etc/openclash/core/clash*
-
