651748d68ec882b2bbf8cd8e6f6d64ff7212ac59
Kwonelee <995546054@qq.com>
2025-10-14 19:32:30 +0800

    Update 01-prepare_package.sh

--- diy/01-prepare_package.sh
+++ diy/01-prepare_package.sh
@@ -17,21 +17,6 @@ git clone https://github.com/Kwonelee/default-settings package/new/default-setti
 # linkease
 git clone https://github.com/sbwml/package_new_istore package/new/linkease
 
-# luci-app-quickfile
-#git clone https://github.com/sbwml/luci-app-quickfile package/new/quickfile
-
-# luci-app-airplay2
-#git clone https://github.com/sbwml/luci-app-airplay2 package/new/airplay2
-
-# luci-app-webdav
-#git clone https://github.com/sbwml/luci-app-webdav package/new/luci-app-webdav
-
-# ddns - fix boot
-#sed -i '/boot()/,+2d' feeds/packages/net/ddns-scripts/files/etc/init.d/ddns
-
-# nlbwmon - disable syslog
-#sed -i 's/stderr 1/stderr 0/g' feeds/packages/net/nlbwmon/files/nlbwmon.init
-
 # UPnP
 rm -rf feeds/{packages/net/miniupnpd,luci/applications/luci-app-upnp}
 git clone https://git.kejizero.online/zhao/miniupnpd feeds/packages/net/miniupnpd -b v2.3.9
@@ -88,25 +73,6 @@ sed -i 's/cheaper = 1/cheaper = 2/g' feeds/packages/net/uwsgi/files-luci-support
 sed -i 's/option timeout 30/option timeout 60/g' package/system/rpcd/files/rpcd.config
 sed -i 's#20) \* 1000#60) \* 1000#g' feeds/luci/modules/luci-base/htdocs/luci-static/resources/rpc.js
 
-# frpc
-#sed -i 's/procd_set_param stdout $stdout/procd_set_param stdout 0/g' feeds/packages/net/frp/files/frpc.init
-#sed -i 's/procd_set_param stderr $stderr/procd_set_param stderr 0/g' feeds/packages/net/frp/files/frpc.init
-#sed -i 's/stdout stderr //g' feeds/packages/net/frp/files/frpc.init
-#sed -i '/stdout:bool/d;/stderr:bool/d' feeds/packages/net/frp/files/frpc.init
-#sed -i '/stdout/d;/stderr/d' feeds/packages/net/frp/files/frpc.config
-#sed -i 's/env conf_inc/env conf_inc enable/g' feeds/packages/net/frp/files/frpc.init
-#sed -i "s/'conf_inc:list(string)'/& \\\\/" feeds/packages/net/frp/files/frpc.init
-#sed -i "/conf_inc:list/a\\\t\t\'enable:bool:0\'" feeds/packages/net/frp/files/frpc.init
-#sed -i '/procd_open_instance/i\\t\[ "$enable" -ne 1 \] \&\& return 1\n' feeds/packages/net/frp/files/frpc.init
-#patch -p1 < ../OpenBox/luci/applications/luci-app-frpc/001-luci-app-frpc-hide-token.patch
-#patch -p1 < ../OpenBox/luci/applications/luci-app-frpc/002-luci-app-frpc-add-enable-flag.patch
-
-# natmap
-#sed -i 's/log_stdout:bool:1/log_stdout:bool:0/g;s/log_stderr:bool:1/log_stderr:bool:0/g' feeds/packages/net/natmap/files/natmap.init
-#pushd feeds/luci
-#patch -p1 <../../../OpenBox/luci/applications/luci-app-natmap/0001-luci-app-natmap-add-default-STUN-server-lists.patch
-#popd
-
 # samba4 - bump version
 rm -rf feeds/packages/net/samba4
 git clone https://github.com/sbwml/feeds_packages_net_samba4 feeds/packages/net/samba4
@@ -124,57 +90,15 @@ sed -i 's/0666/0644/g;s/0744/0755/g;s/0777/0755/g' feeds/luci/applications/luci-
 sed -i 's/0666/0644/g;s/0777/0755/g' feeds/packages/net/samba4/files/samba.config
 sed -i 's/0666/0644/g;s/0777/0755/g' feeds/packages/net/samba4/files/smb.conf.template
 
-# aria2 & ariaNG
-#rm -rf feeds/packages/net/ariang
-#rm -rf feeds/luci/applications/luci-app-aria2
-#git clone https://github.com/sbwml/ariang-nginx package/new/ariang-nginx
-#rm -rf feeds/packages/net/aria2
-#git clone https://github.com/sbwml/feeds_packages_net_aria2 -b 22.03 feeds/packages/net/aria2
-
-# airconnect
-#git clone https://github.com/sbwml/luci-app-airconnect package/new/airconnect --depth=1
-
 # netkit-ftp
 git clone https://github.com/sbwml/package_new_ftp package/new/ftp
 
 # openlist
 #git clone https://github.com/sbwml/luci-app-openlist2 package/new/openlist --depth=1
 
-# qBittorrent
-#git clone https://github.com/sbwml/luci-app-qbittorrent package/new/qbittorrent --depth=1
-
-# unblockneteasemusic
-#git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic package/new/luci-app-unblockneteasemusic --depth=1
-#sed -i 's/解除网易云音乐播放限制/网易云音乐解锁/g' package/new/luci-app-unblockneteasemusic/root/usr/share/luci/menu.d/luci-app-unblockneteasemusic.json
-
-# Mosdns
-#git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/new/mosdns --depth=1
-
-# OpenAppFilter
-#git clone https://github.com/sbwml/OpenAppFilter --depth=1 package/new/OpenAppFilter -b v6
-
-# iperf3
-#sed -i "s/D_GNU_SOURCE/D_GNU_SOURCE -funroll-loops/g" feeds/packages/net/iperf3/Makefile
-
-# nlbwmon
-#sed -i 's/services/network/g' feeds/luci/applications/luci-app-nlbwmon/root/usr/share/luci/menu.d/luci-app-nlbwmon.json
-#sed -i 's/services/network/g' feeds/luci/applications/luci-app-nlbwmon/htdocs/luci-static/resources/view/nlbw/config.js
-
-# mentohust
-#git clone https://github.com/sbwml/luci-app-mentohust package/new/mentohust
-
 # luci-compat - fix translation
 sed -i 's/<%:Up%>/<%:Move up%>/g' feeds/luci/modules/luci-compat/luasrc/view/cbi/tblsection.htm
 sed -i 's/<%:Down%>/<%:Move down%>/g' feeds/luci/modules/luci-compat/luasrc/view/cbi/tblsection.htm
-
-# frpc translation
-#sed -i 's,发送,Transmission,g' feeds/luci/applications/luci-app-transmission/po/zh_Hans/transmission.po
-#sed -i 's,frp 服务器,Frp 服务器,g' feeds/luci/applications/luci-app-frps/po/zh_Hans/frps.po
-#sed -i 's,frp 客户端,Frp 客户端,g' feeds/luci/applications/luci-app-frpc/po/zh_Hans/frpc.po
-
-# luci-app-sqm
-#rm -rf feeds/luci/applications/luci-app-sqm
-#git clone https://git.kejizero.online/zhao/luci-app-sqm feeds/luci/applications/luci-app-sqm
 
 # unzip
 rm -rf feeds/packages/utils/unzip
