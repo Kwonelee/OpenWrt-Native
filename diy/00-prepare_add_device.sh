e5b670a493574ec821b75a748e12977eb38fe969
Kwonelee <995546054@qq.com>
2025-10-25 20:21:56 +0800

    Update 00-prepare_add_device.sh

--- diy/00-prepare_add_device.sh
+++ diy/00-prepare_add_device.sh
@@ -1,11 +1,21 @@
 #!/bin/bash -e
+#===============================================
+# Description: DIY script
+# File name: diy-script.sh
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#===============================================
 
 # 集成设备无线
 mkdir -p package/base-files/files/lib/firmware/brcm
 cp -a $GITHUB_WORKSPACE/configfiles/firmware/brcm/* package/base-files/files/lib/firmware/brcm/
 
+# 替换package/boot/uboot-rockchip/Makefile
+cp -f $GITHUB_WORKSPACE/configfiles/uboot/Makefile package/boot/uboot-rockchip/Makefile
+
 # ================================================================
-# 移植RK3399示例(针对官方完全不支持的设备。采用补丁方式)
+# 移植RK3399示例
 # ================================================================
 # 增加tvi3315a设备
 echo -e "\\ndefine Device/tvi_tvi3315a
@@ -13,25 +23,22 @@ echo -e "\\ndefine Device/tvi_tvi3315a
   DEVICE_MODEL := TVI3315A
   SOC := rk3399
   UBOOT_DEVICE_NAME := tvi3315a-rk3399
+  BOOT_FLOW := pine64-bin
 endef
 TARGET_DEVICES += tvi_tvi3315a" >> target/linux/rockchip/image/armv8.mk
 
-# 替换package/boot/uboot-rockchip/Makefile
-cp -f $GITHUB_WORKSPACE/configfiles/uboot-rockchip/Makefile package/boot/uboot-rockchip/Makefile
+# 复制U-Boot文件到package/boot/uboot-rockchip/patches
+cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3399/rk3399-tvi3315a.dts package/boot/uboot-rockchip/src/arch/arm/dts/
+cp -f $GITHUB_WORKSPACE/configfiles/uboot/rk3399-tvi3315a-u-boot.dtsi package/boot/uboot-rockchip/src/arch/arm/dts/
+cp -f $GITHUB_WORKSPACE/configfiles/uboot/tvi3315a-rk3399_defconfig package/boot/uboot-rockchip/src/configs/
+cp -f $GITHUB_WORKSPACE/configfiles/uboot/900-arm-add-dts-files.patch package/boot/uboot-rockchip/patches/900-arm-add-dts-files.patch
 
-# 复制U-Boot补丁到package/boot/uboot-rockchip/patches
-cp -f $GITHUB_WORKSPACE/configfiles/patch/001-disable-efi-loader.patch package/boot/uboot-rockchip/patches/
-cp -f $GITHUB_WORKSPACE/configfiles/patch/001-rockchip-saradc-supports-rk3568.patch package/boot/uboot-rockchip/patches/
-cp -f $GITHUB_WORKSPACE/configfiles/patch/101-rockchip-add-TVI-TVI3315A.patch package/boot/uboot-rockchip/patches/
-
-# 复制内核补丁到target/linux/rockchip/patches-6.6
-cp -f $GITHUB_WORKSPACE/configfiles/patch/128-arm64-dts-rockchip-rk3399-Add-TVI3315A.patch target/linux/rockchip/patches-6.6/
-# ================================================================
-# RK3399示例结束
-# ================================================================
+# 复制内核文件到target/linux/rockchip/patches-6.6
+cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3399/rk3399-tvi3315a.dts target/linux/rockchip/files/arch/arm64/boot/dts/rockchip/
+cp -f $GITHUB_WORKSPACE/configfiles/target/900-arm64-boot-add-dts-files.patch target/linux/rockchip/patches-6.6/900-arm64-boot-add-dts-files.patch
 
 # ================================================================
-# 移植RK3566示例(针对官方内核支持，但固件不支持的设备。采用文件替换方式)
+# 移植RK3566示例
 # ================================================================
 # 增加station-m2设备
 echo -e "\\ndefine Device/firefly_station-m2
@@ -41,20 +48,15 @@ echo -e "\\ndefine Device/firefly_station-m2
   DEVICE_DTS := rockchip/rk3566-roc-pc
   SUPPORTED_DEVICES += firefly,station-m2 firefly,rk3566-roc-pc
   UBOOT_DEVICE_NAME := station-m2-rk3566
+  BOOT_FLOW := pine64-img
   DEVICE_PACKAGES := kmod-nvme kmod-scsi-core
 endef
 TARGET_DEVICES += firefly_station-m2" >> target/linux/rockchip/image/armv8.mk
 
-# 复制uboot配置、dts到package/boot/uboot-rockchip
-mkdir -p package/boot/uboot-rockchip/src/arch/arm/dts
-mkdir -p package/boot/uboot-rockchip/src/configs
+# 复制U-Boot文件到package/boot/uboot-rockchip/patches
 cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3568/rk3566-roc-pc.dts package/boot/uboot-rockchip/src/arch/arm/dts/
-cp -f $GITHUB_WORKSPACE/configfiles/uboot-rockchip/rk3566-station-m2-u-boot.dtsi package/boot/uboot-rockchip/src/arch/arm/dts/
-cp -f $GITHUB_WORKSPACE/configfiles/uboot-rockchip/station-m2-rk3566_defconfig package/boot/uboot-rockchip/src/configs/
+cp -f $GITHUB_WORKSPACE/configfiles/uboot/rk3566-station-m2-u-boot.dtsi package/boot/uboot-rockchip/src/arch/arm/dts/
+cp -f $GITHUB_WORKSPACE/configfiles/uboot/station-m2-rk3566_defconfig package/boot/uboot-rockchip/src/configs/
 
-# 复制dts到files/arch/arm64/boot/dts/rockchip
-mkdir -p target/linux/rockchip/files/arch/arm64/boot/dts/rockchip
+# 复制内核文件到target/linux/rockchip/patches-6.6
 cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3568/rk3566-roc-pc.dts target/linux/rockchip/files/arch/arm64/boot/dts/rockchip/
-# ================================================================
-# RK35xx示例结束
-# ================================================================
